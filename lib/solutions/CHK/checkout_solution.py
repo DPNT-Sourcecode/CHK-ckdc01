0c9a6a8eff604a08e5225679a298eb9377d98888
sbx_user1051 <sbx_user1051@169.254.17.77>
2022-03-09 17:45:53 +0000

    Wed Mar 09 17:45:53 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,26 @@
-
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+PRICE_TABLE = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
+
+OFFERS = {
+    ('A', 3): 130,
+    ('B', 2): 45
+}
+
 def checkout(skus):
-    raise NotImplementedError()
+    total = 0
+    counter = Counter(skus)
+    print(counter)
+
+if __name__ == '__main__':
+    checkout('ABCA')
+
+
